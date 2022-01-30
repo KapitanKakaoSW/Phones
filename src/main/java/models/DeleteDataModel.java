48eede212a54ad56f89ae5b98788634f037fecfe
KapitanKakao <kapitan.kakao52375@gmail.com>
2022-01-30 14:24:07 +0200

    Upd DeleteDataModel

--- src/main/java/models/DeleteDataModel.java
+++ src/main/java/models/DeleteDataModel.java
@@ -1,4 +1,28 @@
 package models;
 
+import database.DBCheck;
+import database.DBConn;
+import database.entities.Contact;
+import utils.Constants;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DeleteDataModel {
+
+    public static void deleteData(Contact contact) {
+
+        if (DBCheck.isDBExists()) {
+            String sql = "DELETE FROM " + Constants.TABLE_NAME + " WHERE id = ?";
+
+            try (PreparedStatement stmt = DBConn.connect().prepareStatement(sql)) {
+                stmt.setInt(1, contact.getId());
+                stmt.executeUpdate();
+                System.out.println();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else System.out.println(Constants.DB_ABSENT_MSG);
+    }
 }
